d3acb535ce7cc10537d89f64c3a28a17d71f8e91
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-07-24 11:32:38 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# enhancedAnnotations
-NVDA add-on to improve support for annotations.
+# Enhanced Annotations #
+*	Authors: George Kerscher, Noelia Ruiz Martínez
+*	Download [stable version][1] (compatible with NVDA 2022.1 and beyond)
+
+In the DAISY Consortium, best practices are developed for publishers and authors for providing extended (long) descriptions.
+
+The best practices use the HTML details element that follows the image, or a link to another file that contains the extended description.
+
+In both options, the user would need to move to the details or the link and activate it.
+
+Having a keystroke to put focus on the details or the link is ideal. 
+
+Our best practices recommend that the details or the link immediately follows the image, and if the link is followed, a back link to the exact location must be provided. This makes it certain that the user will not get lost.
+
+However it is probable that authors in the wild will place the extended (long) description almost anywhere. In these cases, the user would want to return to the image and hence the need for a way to return to the original image.
+
+This add-on provides both features, in support of this [issue opened in NVDA's repository][2].
+
+## Commands ##
+
+* NVDA+alt+d: moves the cursor to the element identified with aria-details.
+* NVDA+alt+shift+d: moves the cursor to the original element, for example, an image with furter details like a long description.
+
+The above commands can be modified from NVDA's menu, Preferences submenu, Browse mode category.
+
---
+++ addon/globalPlugins/enhancedAnnotations/__init__.py
@@ -0,0 +1,81 @@
+# -*- coding: UTF-8 -*-
+
+# enhancedAnnotations: add ability to move to additional details and go back to original object
+# https://github.com/nvaccess/nvda/issues/13940
+# Copyright (C) 2022 Noelia Ruiz Martínez
+# Released under GPL 2
+
+import api
+import textInfos
+import ui
+import speech
+import inputCore
+import globalPluginHandler
+from logHandler import log
+from browseMode import BrowseModeDocumentTreeInterceptor
+from controlTypes import OutputReason
+from scriptHandler import script
+import addonHandler
+
+from skipTranslation import translate
+
+addonHandler.initTranslations()
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
+	scriptCategory = inputCore.SCRCAT_BROWSEMODE
+
+	def __init__(self):
+		super().__init__()
+		self._lastObjWithDetails = None
+
+	def terminate(self):
+		self._lastObjWithDetails = None
+
+	@script(
+		# Translators: message presented in input help mode.
+		description=_("Navigates to the details of the current position in browse mode"),
+		gesture="kb:NVDA+alt+d"
+	)
+	def script_moveToRelatedDetails(self, gesture):
+		focus = api.getFocusObject()
+		treeInterceptor = focus.treeInterceptor
+		if not isinstance(treeInterceptor, BrowseModeDocumentTreeInterceptor) or treeInterceptor.passThrough:
+			return
+		curObj = treeInterceptor.currentNVDAObject
+		if curObj.hasDetails:
+			relatedDetails = curObj.detailsRelations
+		else:
+			# Message translated in NVDA's core.
+			ui.message(translate("No additional details"))
+			return
+		for target in relatedDetails:
+			info = treeInterceptor.makeTextInfo(target)
+			info.collapse()
+			treeInterceptor._set_selection(info, reason=OutputReason.QUICKNAV)
+			speech.speakObject(treeInterceptor.currentNVDAObject, reason=OutputReason.QUICKNAV)
+			self._lastObjWithDetails = curObj
+			return
+
+	@script(
+		# Translators: message presented in input help mode.
+		description=_("After navigating from an object to additional details, moves back to the original position in browse mode"),
+		gesture="kb:NVDA+shift+alt+d"
+	)
+	def script_moveBackToObjWithDetails(self, gesture):
+		focus = api.getFocusObject()
+		treeInterceptor = focus.treeInterceptor
+		if not isinstance(treeInterceptor, BrowseModeDocumentTreeInterceptor) or treeInterceptor.passThrough:
+			return
+		if self._lastObjWithDetails is None:
+			# Translators: message presented when there's no original object to go back from additional details.
+			ui.message(_("No element with additional details to go back"))
+			return
+		try:
+			info = treeInterceptor.makeTextInfo(self._lastObjWithDetails)
+			info.collapse()
+			treeInterceptor._set_selection(info, reason=OutputReason.QUICKNAV)
+			speech.speakObject(treeInterceptor.currentNVDAObject, reason=OutputReason.QUICKNAV)
+		except LookupError as e:
+			log.debugWarning("enhancedAnnotations add-on: error moving to object with details", exc_info=True)
+			raise e
